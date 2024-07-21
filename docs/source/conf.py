81a8169f6c58c1299f7d72291fc102c60515d2fb
Asa Gilmore <asaagilmore@gmail.com>
2024-07-21 15:23:39 -0700

    fix sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,16 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_ivar = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+
 templates_path = ['_templates']
 exclude_patterns = []
 
